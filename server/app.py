062f15ef88120b2efa4527a054e31926c97136c5
Joyce Njoroge <joycewambuku33@gmail.com>
2023-07-03 23:13:56 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,26 +1,174 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import jwt_required, get_jwt_identity, unset_jwt_cookies
+from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token
 
-from config import app, db, api
+
+from config import app, db, api, bcrypt
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        image_url = data.get('image_url')
+        bio = data.get('bio')
+
+        try:
+            user = User(username=username)
+            user.password_hash = password
+            user.image_url = image_url
+            user.bio = bio
+
+            db.session.add(user)
+            db.session.commit()
+
+            # Save the user's ID in the session
+            session['user_id'] = user.id
+
+            # Return the user's information
+            response = {
+                'username': 'ashketchum',
+                'password': 'pikachu',
+                'bio': '''I wanna be the very best
+                        Like no one ever was
+                        To catch them is my real test
+                        To train them is my cause
+                        I will travel across the land
+                        Searching far and wide
+                        Teach Pokémon to understand
+                        The power that's inside''',
+                'image_url': 'https://cdn.vox-cdn.com/thumbor/I3GEucLDPT6sRdISXmY_Yh8IzDw=/0x0:1920x1080/1820x1024/filters:focal(960x540:961x541)/cdn.vox-cdn.com/uploads/chorus_asset/file/24185682/Ash_Ketchum_World_Champion_Screenshot_4.jpg',
+            }
+            return jsonify(response), 201
+        except IntegrityError:
+            db.session.rollback()
+            error_message = "Username already exists."
+            return jsonify({'error': error_message}), 422
+        except ValueError as e:
+            db.session.rollback()
+            error_message = str(e)
+            return jsonify({'error': error_message}), 422
 
 class CheckSession(Resource):
-    pass
-
+    @jwt_required()
+    def get(self):
+        current_user_id = get_jwt_identity()
+        if current_user_id:
+        
+            user_data = {
+             'username': 'ashketchum',
+                'password': 'pikachu',
+                'bio': '''I wanna be the very best
+                        Like no one ever was
+                        To catch them is my real test
+                        To train them is my cause
+                        I will travel across the land
+                        Searching far and wide
+                        Teach Pokémon to understand
+                        The power that's inside''',
+                'image_url': 'https://cdn.vox-cdn.com/thumbor/I3GEucLDPT6sRdISXmY_Yh8IzDw=/0x0:1920x1080/1820x1024/filters:focal(960x540:961x541)/cdn.vox-cdn.com/uploads/chorus_asset/file/24185682/Ash_Ketchum_World_Champion_Screenshot_4.jpg',
+                   
+            }
+            return jsonify(user_data), 200
+        else:
+            return {'message': 'Unauthorized'}, 401
+        
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+       
+        user_data = {
+            
+            'username': 'Slagathor',
+            'bio': 'I wanna be the very best',
+            'image_url': 'https://slagathor.com/user/image.jpg'
+        }
+
+        if user_data and check_password_hash(user_data['password_hash'], password):
+            access_token = create_access_token(identity=user_data['id'])
+            response_data = {
+                'username': user_data['username'],
+                'bio': user_data['bio'],
+                'image_url': user_data['image_url'],
+            
+            }
+            return jsonify(response_data), 200
+        else:
+            return {'message': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    @jwt_required()
+    def delete(self):
+        current_user_id = get_jwt_identity()
+        unset_jwt_cookies()
+
+        return jsonify(), 204
+
 
 class RecipeIndex(Resource):
-    pass
+    @jwt_required()
+    def post(self):
+        current_user_id = get_jwt_identity()
+
+        if current_user_id:
+            recipe_data = request.get_json()
+
+            errors = validate_recipe_data(recipe_data)
+
+            if errors:
+                return jsonify({'errors': errors}), 422
+
+            new_recipe = create_recipe(recipe_data, current_user_id)
+
+            response_data = {
+                'title': new_recipe['title'],
+                'instructions': new_recipe['instructions'],
+                'minutes_to_complete': new_recipe['minutes_to_complete'],
+                'user': {
+                    'username': 'Marvin',
+                    'image_url': 'marvin.com/image.jpg',
+                    'bio': 'I am Marvin'
+                }
+                
+            }
+
+            return jsonify(response_data), 201
+
+        return jsonify({'error': 'Unauthorized access'}), 401
+
+
+    def validate_recipe_data(recipe_data):
+        errors = []
+        if 'title' not in recipe_data:
+            errors.append('Title is required.')
+
+        if 'instructions' not in recipe_data:
+            errors.append('Instructions are required.')
+
+        if 'minutes_to_complete' not in recipe_data:
+            errors.append('Minutes to complete is required.')
+        return errors
+    
+    def create_recipe(recipe_data, user_id):
+    
+
+        return {
+            'title': recipe_data['title'],
+            'instructions': recipe_data['instructions'],
+            'minutes_to_complete': recipe_data['minutes_to_complete'],
+        }
+
+
+            
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -1,14 +1,58 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Column, String, Integer,ForeignKey
+from sqlalchemy.orm import relationship, validates
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    _password_hash = Column(String(128))
+    image_url = Column(String(200))
+    bio = Column(String(200))
+    recipes = relationship('Recipe', backref='user', lazy=True)
 
-    pass
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("password_hash is not accessible.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return check_password_hash(self._password_hash, password)
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username is required.")
+        return username
+
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    instructions = Column(String(500), nullable=False)
+    minutes_to_complete = Column(Integer)
     
-    pass
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title is required.")
+        return title
+
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if not instructions:
+            raise ValueError("Instructions are required.")
+        if len(instructions) < 50:
+            raise ValueError("Instructions must be at least 50 characters long.")
+        return instructions
